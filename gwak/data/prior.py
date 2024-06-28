fe18aed18930f125c6f457323f14b6381b6a7bda
Katya <katyag@mit.edu>
2024-06-28 06:47:57 -0700

    fix training scripts

--- gwak/data/prior.py
+++ gwak/data/prior.py
@@ -1,15 +1,17 @@
+from collections import OrderedDict
+
 class Prior():
 
     def __init__(self, **kwargs):
 
-        self.params = dict()
+        self.params = OrderedDict()
         for k, v in kwargs.items():
             print(f'Setting for key {k}, value {v}')
             self.params[k] = v
 
     def sample(self, batch_size):
 
-        self.sampled_params = dict()
+        self.sampled_params = OrderedDict()
 
         for k in self.params.keys():
             self.sampled_params[k] = self.params[k].sample((batch_size,))
--- gwak/train/train/train.py
+++ gwak/train/train/train.py
@@ -1,12 +1,14 @@
+import argparse
 import numpy as np
 from typing import Callable, List, Optional
 
 import torch
+import torch.optim as optim
 from torch.distributions.uniform import Uniform
 
 import ml4gw
 from ml4gw.dataloading import Hdf5TimeSeriesDataset
-from ml4gw.waveforms import SineGaussian
+import ml4gw.waveforms as waveforms # import SineGaussian
 from ml4gw.transforms import SpectralDensity, Whiten
 from ml4gw.distributions import Cosine, PowerLaw
 from ml4gw.gw import compute_observed_strain, get_ifo_geometry
@@ -15,16 +17,18 @@ from pathlib import Path
 import gwak
 from gwak.data.prior import Prior
 
-from models import LSTM_AE_SPLIT
+import models
 
 
 def main(
-    data_dir: str = '/home/ethan.marx/amplfi/data/train/background',
+    data: torch.nn.Module,
+    model_name: str,
+    data_dir: str = '/home/ethan.marx/aframe/data/train/background',
     ifos: List = ['H1', 'L1'],
-    sample_rate: int = 4096,
+    sample_rate: int = 2048,
     psd_length: int = 64, # segment length in seconds to estimate PSD
-    batch_size: int = 1,
-    kernel_length: float = 200/4096, # number of sec passed in,
+    batch_size: int = 128,
+    kernel_length: float = 200/2048, # number of sec passed in,
     fftlength: float = 2,
     fduration: float = 1, # whitening parameter: how much data to cut off,
 ):
@@ -33,15 +37,21 @@ def main(
     print(f'Running training on {data_dir}')
 
     kernel_size = (psd_length + fduration + kernel_length) * sample_rate
+    batches_per_epoch = 128
 
     # create training dataloader
     dataloader = Hdf5TimeSeriesDataset(
-        fnames = list(data_dir.glob('*.hdf5')),
+        fnames = list([data_dir/'background-1240700690-20000.hdf5']),
         channels = ifos,
         kernel_size = int(kernel_size),
         batch_size = batch_size,
-        batches_per_epoch = 200,
+        batches_per_epoch = batches_per_epoch, # ??
         coincident = False
+    )
+
+    num_workers = 5
+    dataloader = torch.utils.data.DataLoader(
+        dataloader, num_workers=num_workers, pin_memory=False
     )
 
     # psd estimator
@@ -50,16 +60,16 @@ def main(
         sample_rate,
         fftlength,
         average = "median"
-    )
+    ).to('cuda')
 
     # create whitener
     whitener = Whiten(
         fduration,
         sample_rate,
         highpass = 30,
-    )
+    ).to('cuda')
 
-    sine_gaussian = SineGaussian(sample_rate, duration=kernel_length + fduration)
+    data = getattr(waveforms, data)(sample_rate, duration=kernel_length + fduration)
 
     # something with sample method that returns dict that maps
     # parameter name to tensor of parameter names
@@ -77,49 +87,108 @@ def main(
       psi = Uniform(0, 2 * torch.pi)
     )
 
-    model = LSTM_AE_SPLIT(len(ifos), 200, 4)
+    model_class = getattr(models, model_name)
+    model = model_class(len(ifos), 200, 8).to('cuda')
 
-    for X in dataloader:
-      # X is shape (batch_size, num_ifos, kernel_size)
+    loss_fn = torch.nn.L1Loss()
+    optimizer = optim.Adam(model.parameters())
 
-      # split X into psd data and data to be whitened
-      split_size = int((kernel_length + fduration) * sample_rate)
-      splits = [X.size(-1) - split_size, split_size]
-      psd_data, X = torch.split(X, splits, dim=-1)
+    training_history = {
+        'train_loss': []    }
 
-      # calculate psds
-      psds = spectral_density(psd_data)
+    # training loop
+    epoch_count = 0
+    EPOCHS = 200
 
+    for epoch_num in range(EPOCHS):
+        epoch_count += 1
+        epoch_train_loss = 0
+        print(f'Epoch {epoch_count}')
+        for X in dataloader:
+            X = X[0].to('cuda')
+            optimizer.zero_grad()
+            # X is shape (batch_size, num_ifos, kernel_size)
 
-      # sample from prior and generate waveforms
-      parameters = intrinsic_prior.sample(batch_size) # dict[str, torch.tensor]
-      cross, plus = sine_gaussian(**parameters)
+            # split X into psd data and data to be whitened
+            split_size = int((kernel_length + fduration) * sample_rate)
+            splits = [X.size(-1) - split_size, split_size]
+            psd_data, X = torch.split(X, splits, dim=-1)
 
-      # sample extrinsic parameters
-      ra, dec, psi = extrinsic_prior.sample(batch_size)
+            # calculate psds
+            psds = spectral_density(psd_data.double())
 
-      # get detector orientations
-      tensors, vertices = get_ifo_geometry(*ifos)
+            # sample from prior and generate waveforms
+            parameters = intrinsic_prior.sample(batch_size) # dict[str, torch.tensor]
+            cross, plus = data(**parameters)
 
-      # compute detector responses
-      responses = compute_observed_strain(
-          dec,
-          psi,
-          ra,
-          tensors,
-          vertices,
-          sample_rate,
-          cross=cross,
-          plus=plus
-      )
+            # sample extrinsic parameters
+            ra, dec, psi = extrinsic_prior.sample(batch_size).values()
 
-      # inject into data and whiten
-      injected = X + responses
-      whitened = whitener(X, psds)
+            # get detector orientations
+            tensors, vertices = get_ifo_geometry(*ifos)
 
-      loss = model(whitened)
-      loss.backward()
+            # compute detector responses
+            responses = compute_observed_strain(
+              dec,
+              psi,
+              ra,
+              tensors,
+              vertices,
+              sample_rate,
+              cross=cross.float(),
+              plus=plus.float()
+            ).to('cuda')
 
+            # inject into data and whiten
+            injected = X + responses
+
+            whitened = whitener(injected.double(), psds.double())
+
+            # normalize the input data
+            stds = torch.std(whitened, dim=-1, keepdim=True)
+            whitened = whitened / stds
+
+            response = model(whitened)
+            loss = loss_fn(model(whitened), whitened)
+            epoch_train_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+
+        epoch_train_loss /= batches_per_epoch
+        print(f'Epoch loss is {epoch_train_loss}')
+        training_history['train_loss'].append(epoch_train_loss)
+
+    # save the model
+    torch.save(model.state_dict(), f'model.pt')
+
+    # plot training history
+    from matplotlib import pyplot as plt
+    epochs = np.linspace(1, epoch_count, epoch_count)
+    plt.plot(epochs, np.array(training_history[
+            'train_loss']), label='Training loss')
+    plt.legend()
+    plt.xlabel('Epochs', fontsize=15)
+    plt.ylabel('Loss', fontsize=15)
+    plt.grid()
+    plt.savefig('training_loss.pdf')
+    plt.clf()
+
+    plt.plot(model(whitened)[0,0,:].cpu().detach().numpy(), label='reco')
+    plt.plot(whitened[0,0,:].cpu().detach().numpy(), label='reco')
+    plt.legend()
+    plt.grid()
+    plt.savefig('reconstruction.pdf')
+    plt.clf()
 
 if __name__=='__main__':
-    main()
+
+    parser = argparse.ArgumentParser()
+
+    # Required arguments
+    parser.add_argument('--data', type=str, help='Directory containing the injections to evaluate')
+    parser.add_argument('--model', type=str,
+                        help='Directory containing the injections to evaluate')
+
+    args = parser.parse_args()
+
+    main(args.data, args.model)
